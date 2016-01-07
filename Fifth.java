77e2b15d28aa7d172844890b07839711b92e1939
Alexrut <alexrutr@gmail.com>
2016-01-07 19:15:09 +0500

    удален коментарий

--- Fifth.java
+++ Fifth.java
@@ -5,7 +5,7 @@ public class Fifth {
     int c =++b + a++;           // 6+12+1=19 true answer is 18 b=b+1(6) c=b+a(18) a=a+1(13)
     System.out.println(c);
     System.out.println(a);
-    int d =b++ * 2;             //12 Its true
+    int d =b++ * 2;             //12 
     System.out.println(d);
   }
 }
