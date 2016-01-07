bda89921534038e8d7197f5fb78a5385012cc545
Alexrut <alexrutr@gmail.com>
2016-01-07 18:51:54 +0500

    ex5.1

---
+++ Fifth1.java
@@ -0,0 +1,9 @@
+public class Fifth1 {
+   public static void main (String [] arg) {
+      boolean a = true;
+      boolean b = false;
+      boolean c = true;
+      boolean result = a && (b || c);
+      System.out.println(result);
+   }
+}
